c0b8732f5675bfdb95fa24441e7912018de42151
Matheus de Paula <matheusdev20@gmail.com>
2020-04-01 20:47:06 -0300

    Adding cors policy

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const graphqlHttp = require('express-graphql');
 const grqlSchema = require('./graphql/Schema/index')
 const grqlResolvers = require('./graphql/Resolvers/index')
 const isAuth = require('./middleware/is-auth')
+const cors = require('cors')
 require('./config/mongodb')
 const app = express();
 app.use(bodyParser.json());
+app.use(cors())
 app.use((req,res,next) =>{
     res.setHeader('Access-Control-Allow-Origin', '*')
     res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS')
